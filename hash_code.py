c93122ce6e19b3e82ce0b575b85f3f424ce8b974
ttn2007 <91877255+ttn2007@users.noreply.github.com>
2025-05-14 12:03:36 +0330

    Update and rename create hash code.py to hash_code.py

--- hash_code.py
+++ hash_code.py
@@ -1,8 +1,6 @@
 import hashlib
 
-# تابع برای تولید هش
 def generate_hash(input_string, algorithm='sha256'):
-    # انتخاب الگوریتم هش
     if algorithm == 'md5':
         hash_object = hashlib.md5()
     elif algorithm == 'sha1':
@@ -12,18 +10,14 @@ def generate_hash(input_string, algorithm='sha256'):
     else:
         return "الگوریتم هش نامعتبر است."
     
-    # تبدیل داده به بایت‌ها و تولید هش
     hash_object.update(input_string.encode())
     return hash_object.hexdigest()
 
-# دریافت ورودی از کاربر
 input_string = input("لطفاً یک رشته برای تولید هش وارد کنید: ")
 
-# انتخاب الگوریتم هش از کاربر
 print("الگوریتم‌های موجود: md5, sha1, sha256")
 algorithm = input("الگوریتم هش مورد نظر را انتخاب کنید: ").lower()
 
-# تولید هش
 hashed_string = generate_hash(input_string, algorithm)
 
 print(f"هش تولید شده با الگوریتم {algorithm}: {hashed_string}")
